aa35a3fedc5784c90d87e5ce267b37167b427c89
Madi McDougall <mmcdouga9466@scrippscollege.edu>
2023-02-06 15:17:13 +0100

    player controller + camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    private float offsetY = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (pos.y - player.position.y <= offsetY)
+        {
+            pos.y = player.position.y + offsetY;
+        }
+        transform.position = pos;
+    }
+}
