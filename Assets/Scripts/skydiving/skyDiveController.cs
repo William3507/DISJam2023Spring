808130f60a769151ac96396e285047faa939c116
williambailey <wbaileyfletcher@gmail.com>
2023-02-07 12:32:46 +0100

    Auto stash before merge of "main" and "origin/main"
    Almost done!

--- Assets/Scripts/skydiving/skyDiveController.cs
+++ Assets/Scripts/skydiving/skyDiveController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class skyDiveController : MonoBehaviour
 {
@@ -10,6 +11,13 @@ public class skyDiveController : MonoBehaviour
     float frameTimer;
     int currentFrame;
     private Rigidbody2D rb2D;
+    public Sprite deathSprite;
+    public GameObject forestBackground;
+    public bool isHit = false;
+    public float deathTimer = 4;
+    bool hasSpawned = false;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +29,112 @@ public class skyDiveController : MonoBehaviour
         currentFrame = 0;
     }
 
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        isHit = true;
+
+        if (col.gameObject.CompareTag("Obstacle"))
+        {
+            onDeath(mySpriteRenderer, deathSprite);
+        }
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        Vector2 vel = rb2D.velocity;
+
+        if (!isHit)
+        {
+            changeCostume();
+
+            //Controlling downward movement
+
+            vel.y = -5;
+
+
+            //Change direction when space is pressed
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (vel.x == 0)
+                {
+                    vel.x = 4;
+                }
+                else
+                {
+                    vel.x *= -1;
+                }
+
+                if (vel.x > 0)
+                {
+                    mySpriteRenderer.flipX = false;
+                }
+                else
+                {
+                    mySpriteRenderer.flipX = true;
+                }
+            }
+
+            //keep from going out of bounds
+            if (transform.position.x >= 9)
+            {
+                vel.x = -4;
+                mySpriteRenderer.flipX = true;
+
+            }
+            if (transform.position.x <= -9)
+            {
+                vel.x = 4;
+                mySpriteRenderer.flipX = false;
+            }
+            rb2D.velocity = vel;
+        }
+        else
+        {
+            //Get Sprite back to center
+
+            if(transform.position.x < -1)
+            {
+                vel.x = 4;
+            }
+            else if(transform.position.x > 1)
+            {
+                vel.x = -4;
+            }
+            else
+            {
+                vel.x = 0;
+               
+            }
+
+
+            if (vel.x == 0 && hasSpawned == false)
+            {
+                Vector3 spawnPosition = transform.position;
+                spawnPosition += new Vector3(0, -20, 0);
+                // the Instatiate function creates a new GameObject copy (clone) from a Prefab at a specific location and orientation.
+                Instantiate(forestBackground, spawnPosition, Quaternion.identity);
+
+                hasSpawned = true;
+            }
+
+
+            if (hasSpawned)
+            {
+                deathTimer -= Time.deltaTime;
+                if(frameTimer <= 0)
+                {
+                    vel.y = 0;
+                }
+            }
+
+            rb2D.velocity = vel;
+
+        }
+
+    }
+
+    public void changeCostume()
     {
 
         frameTimer -= Time.deltaTime;
@@ -36,48 +148,16 @@ public class skyDiveController : MonoBehaviour
                 currentFrame = 0;
             }
         }
+    }
 
+    void onDeath(SpriteRenderer mySpriteRenderer, Sprite deathSprite)
+    {
+        mySpriteRenderer.sprite = deathSprite;
 
-        Vector2 vel = rb2D.velocity;
+        spriteList[0] = deathSprite;
+        spriteList[1] = deathSprite;
+        spriteList[2] = deathSprite;
 
-        vel.y = -5;
-
-        //Change direction when space is pressed
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if(vel.x == 0)
-            {
-                vel.x = 4;
-            }
-            else
-            {
-                vel.x *= -1;
-            }
-
-            if (vel.x > 0)
-            {
-                mySpriteRenderer.flipX = false;
-            }
-            else
-            {
-                mySpriteRenderer.flipX = true;
-            }
-        }
-
-        //keep from going out of bounds
-        if (transform.position.x >= 9)
-        {
-            vel.x = -4;
-            mySpriteRenderer.flipX = true;
-
-        }
-        if (transform.position.x <= -9)
-        {
-            vel.x = 4;
-            mySpriteRenderer.flipX = false;
-        }
-        rb2D.velocity = vel;
-
-
+        
     }
 }
